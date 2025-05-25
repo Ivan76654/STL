1676e8c06f8d7c16b2ce60505253d83b008ebdb0
Ivan Lovrić <ivan.lovric2101@gmail.com>
2025-05-25 23:27:08 +0200

    Add controller class for team endpoints.

---
+++ backend/stl/src/main/java/hr/stl/core/controller/TeamController.java
@@ -0,0 +1,80 @@
+package hr.stl.core.controller;
+
+import hr.stl.core.dto.TeamDto;
+import hr.stl.core.service.TeamService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping("/teams")
+@RequiredArgsConstructor
+public class TeamController {
+
+    private final TeamService teamService;
+
+    @GetMapping
+    public ResponseEntity<?> getAllTeams() {
+        List<TeamDto> teams = teamService.getAllTeams();
+
+        return ResponseEntity.ok(teams);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getTeamById(@PathVariable Long id) {
+        try {
+            TeamDto teamDto = teamService.getTeamById(id);
+
+            return ResponseEntity.ok(teamDto);
+        } catch (NoSuchElementException ex) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<?> insertTeam(@RequestBody TeamDto teamDto) {
+        TeamDto created = teamService.insertTeam(teamDto);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(created.getTeamId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(created);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateTeamById(@PathVariable Long id, @RequestBody TeamDto teamDto) {
+        try {
+            teamService.updateTeamById(id, teamDto);
+
+            return ResponseEntity.noContent().build();
+        } catch (NoSuchElementException ex) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTeamById(@PathVariable Long id) {
+        try {
+            teamService.deleteTeamById(id);
+
+            return ResponseEntity.noContent().build();
+        } catch (NoSuchElementException ex) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+}
